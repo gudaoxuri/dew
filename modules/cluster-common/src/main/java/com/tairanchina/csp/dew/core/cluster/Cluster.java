8c73c09a118101d02c8b078df4c82ffe9bc37efc
gudaoxuri <i@sunisle.org>
2018-07-11 18:20:13 +0800

    Add sleuth by MQ #12

--- modules/cluster-common/src/main/java/com/tairanchina/csp/dew/core/cluster/Cluster.java
+++ modules/cluster-common/src/main/java/com/tairanchina/csp/dew/core/cluster/Cluster.java
@@ -1,6 +1,10 @@
 package com.tairanchina.csp.dew.core.cluster;
 
+import java.util.Map;
 import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * 集群服务
@@ -8,6 +12,22 @@ import java.util.UUID;
 public class Cluster {
 
     public static final String CLASS_LOAD_UNIQUE_FLAG = UUID.randomUUID().toString();
+
+    private static Function<String,Map<String, Object>> _mqGetHeader;
+    private static Consumer<Object[]> _mqSetHeader;
+
+    public static void initMQHeader(Function<String,Map<String, Object>> mqGetHeader, Consumer<Object[]> mqSetHeader) {
+        _mqGetHeader = mqGetHeader;
+        _mqSetHeader = mqSetHeader;
+    }
+
+    public static Map<String, Object> getMQHeader(String name) {
+        return _mqGetHeader.apply(name);
+    }
+
+    public static void setMQHeader(String name,Map<String, Object> header) {
+        _mqSetHeader.accept(new Object[]{name,header});
+    }
 
     /**
      * MQ服务
