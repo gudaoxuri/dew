8240913a2e30f4a15de2a9b4a13be18f4b222870
gudaoxuri <i@sunisle.org>
2019-03-06 17:21:15 +0800

    Start building dew 2.x

--- modules/jdbc-starter/src/main/java/com/tairanchina/csp/dew/jdbc/annotations/Param.java
+++
@@ -1,9 +0,0 @@
-package com.tairanchina.csp.dew.jdbc.annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.PARAMETER)
-public @interface Param {
-    String value() default "";
-}
