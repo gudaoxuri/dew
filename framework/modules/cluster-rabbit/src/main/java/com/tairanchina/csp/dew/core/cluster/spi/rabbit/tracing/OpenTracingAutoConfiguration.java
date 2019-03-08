b1a0e7bb2cf573c474e6ba60a454ccc4c30e1f51
gudaoxuri <i@sunisle.org>
2019-03-08 18:41:16 +0800

    Fix tracing dependency.

--- framework/modules/cluster-rabbit/src/main/java/com/tairanchina/csp/dew/core/cluster/spi/rabbit/tracing/OpenTracingAutoConfiguration.java
+++ framework/modules/cluster-rabbit/src/main/java/com/tairanchina/csp/dew/core/cluster/spi/rabbit/tracing/OpenTracingAutoConfiguration.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Configuration;
@@ -20,17 +21,20 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Configuration
-@ConditionalOnClass({RabbitTemplate.class, Scope.class})
+@ConditionalOnClass(RabbitTemplate.class)
 @ConditionalOnExpression("#{'${dew.cluster.cache}'=='rabbit' || '${dew.cluster.mq}'=='rabbit' || '${dew.cluster.lock}'=='rabbit' || '${dew.cluster.map}'=='rabbit' || '${dew.cluster.election}'=='rabbit'}")
 public class OpenTracingAutoConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(OpenTracingAutoConfiguration.class);
 
-    @Autowired
+    @Autowired(required = false)
     private Tracer tracer;
 
     @PostConstruct
     public void init() {
+        if(tracer==null){
+            return;
+        }
         logger.info("Load Auto Configuration : {}", this.getClass().getName());
         RabbitClusterMQ.setSendBeforeFun((exchange, routingKey, messageProperties) -> {
             try {
@@ -47,15 +51,31 @@ public class OpenTracingAutoConfiguration {
             RabbitMqSpanDecorator.onSend(messageProperties, exchange, routingKey, scope.span());
             return scope;
         });
-        RabbitClusterMQ.setSendErrorFun((ex, beforeResult) -> RabbitMqSpanDecorator.onError(ex, ((Scope) beforeResult).span()));
-        RabbitClusterMQ.setSendFinishFun(beforeResult -> ((Scope) beforeResult).close());
+        RabbitClusterMQ.setSendErrorFun((ex, beforeResult) -> {
+            if (beforeResult != null) {
+                RabbitMqSpanDecorator.onError(ex, ((Scope) beforeResult).span());
+            }
+        });
+        RabbitClusterMQ.setSendFinishFun(beforeResult -> {
+            if (beforeResult != null) {
+                ((Scope) beforeResult).close();
+            }
+        });
         RabbitClusterMQ.setReceiveBeforeFun((exchange, routingKey, queueName, messageProperties) -> {
             Optional<Scope> child = RabbitMqTracingUtils.buildReceiveSpan(messageProperties, tracer);
             child.ifPresent(scope -> RabbitMqSpanDecorator.onReceive(messageProperties, exchange, routingKey, queueName, scope.span()));
             return child;
         });
-        RabbitClusterMQ.setReceiveErrorFun((ex, beforeResult) -> ((Optional<Scope>) beforeResult).ifPresent(scope -> RabbitMqSpanDecorator.onError(ex, scope.span())));
-        RabbitClusterMQ.setReceiveFinishFun(beforeResult -> ((Optional<Scope>) beforeResult).ifPresent(Scope::close));
+        RabbitClusterMQ.setReceiveErrorFun((ex, beforeResult) -> {
+            if (beforeResult != null) {
+                ((Optional<Scope>) beforeResult).ifPresent(scope -> RabbitMqSpanDecorator.onError(ex, scope.span()));
+            }
+        });
+        RabbitClusterMQ.setReceiveFinishFun(beforeResult -> {
+            if (beforeResult != null) {
+                ((Optional<Scope>) beforeResult).ifPresent(Scope::close);
+            }
+        });
 
 
     }
