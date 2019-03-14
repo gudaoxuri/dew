4c4f3a35011126ae37629818e7973afd284e1176
gudaoxuri <i@sunisle.org>
2019-03-14 17:47:50 +0800

    Directory change & code optimization.

---
+++ framework/components/config/src/main/java/com/tairanchina/csp/dew/config/WebSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.tairanchina.csp.dew.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated().and()
+                .httpBasic();
+    }
+
+}
