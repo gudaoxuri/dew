5c2151eed9ba418dbdaff069f4e2dcf12133056e
liuhongcheng <hzlhc@tairanchina.com>
2019-08-29 17:19:49 +0800

    Optimize remote debug of multi-node pod

--- devops/maven/dew-maven-plugin/src/main/resources/dockerfile/jvmservice_springboot/debug-clear.sh
+++ devops/maven/dew-maven-plugin/src/main/resources/dockerfile/jvmservice_springboot/debug-clear.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ps -ef | grep java | grep address=5005 | awk '{print $1}' | xargs kill -9
+pidof java | awk '{print $1}' | xargs kill -9
--- devops/maven/dew-maven-plugin/src/main/resources/dockerfile/jvmservice_springboot/debug-java.sh
+++ devops/maven/dew-maven-plugin/src/main/resources/dockerfile/jvmservice_springboot/debug-java.sh
@@ -3,6 +3,6 @@
 export JAVA_ENABLE_DEBUG=true
 export JAVA_OPTIONS=${JAVA_DEBUG_OPTIONS}
 
-ps -ef | grep java | grep address=5005 | awk '{print $1}' | xargs kill -9
+pidof java | awk '{print $1}' | xargs kill -9
 
 ./run-java.sh
