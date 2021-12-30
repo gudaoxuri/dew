96265ee6c189be43966d67cfdbebcb2efcc25388
nipeixuan <34534268+nipeixuan@users.noreply.github.com>
2021-12-30 16:47:38 +0800

    example回滚

--- examples/cluster-example/src/main/java/group/idealworld/dew/example/cluster/ClusterExampleInitiator.java
+++ examples/cluster-example/src/main/java/group/idealworld/dew/example/cluster/ClusterExampleInitiator.java
@@ -44,12 +44,48 @@ public class ClusterExampleInitiator {
      */
     @PostConstruct
     public void init() throws Exception {
+        // cache
+        Dew.cluster.cache.flushdb();
+        Dew.cluster.cache.del("n_test");
+        assert !Dew.cluster.cache.exists("n_test");
+        Dew.cluster.cache.setex("n_test", "{\"name\":\"jzy\"}", 1);
+        assert Dew.cluster.cache.exists("n_test");
+        assert "jzy".equals($.json.toJson(Dew.cluster.cache.get("n_test")).get("name").asText());
+        Thread.sleep(1000);
+        assert !Dew.cluster.cache.exists("n_test");
+        assert null == Dew.cluster.cache.get("n_test");
+        // ...
+
+        // map
+        ClusterMap<TestMapObj> mapObj = Dew.cluster.map.instance("test_obj_map", TestMapObj.class);
+        mapObj.clear();
+        TestMapObj obj = new TestMapObj();
+        obj.someField = "测试";
+        mapObj.put("test", obj);
+        assert "测试".equals(mapObj.get("test").someField);
+        // ...
+
+        // lock
+        ClusterLock lock = Dew.cluster.lock.instance("test_lock");
+        // tryLock 示例，等待0ms，忘了手工unLock或出异常时1s后自动解锁
+        if (lock.tryLock(0, 1000)) {
+            try {
+                // 已加锁，执行业务方法
+            } finally {
+                // 必须手工解锁
+                lock.unLock();
+            }
+        }
+        // tryLockWithFun 示例
+        lock.tryLockWithFun(0, 1000, () -> {
+            // 已加锁，执行业务方法，tryLockWithFun会将业务方法包裹在try-cache中，无需手工解锁
+        });
 
         // pub-sub
         Dew.cluster.mq.subscribe("test_pub_sub", message ->
                 logger.info("pub_sub>>" + message));
         Thread.sleep(1000);
-        Dew.cluster.mq.publish("test_pub_sub", "msgA",null,true);
+        Dew.cluster.mq.publish("test_pub_sub", "msgA");
         Dew.cluster.mq.publish("test_pub_sub", "msgB");
         // req-resp
         Dew.cluster.mq.response("test_rep_resp", message ->
